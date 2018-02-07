51ff5328f0f26e0c4531ae412d2a71ea4871c826
lebenef <lebenef@ssh4>
2018-02-07 23:55:55 +0100

    CreationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FF\FastBundle\FFFastBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/FF/FastBundle/FFFastBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FF\FastBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FFFastBundle extends Bundle
+{
+}
