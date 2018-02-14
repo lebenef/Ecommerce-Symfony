4b02f4b5b16a8425182533ec068354b8f5ee3b4f
lebenef <kmoukin@gmail.com>
2018-02-14 11:20:48 -0400

    Formulaire

--- src/FF/FastBundle/Controller/GammesProduitsController.php
+++ src/FF/FastBundle/Controller/GammesProduitsController.php
@@ -5,6 +5,14 @@ namespace FF\FastBundle\Controller;
 use FF\FastBundle\Entity\GammeProduit;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use OC\PlatformBundle\Entity\Advert;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class GammesProduitsController extends Controller
 {
@@ -18,6 +26,22 @@ class GammesProduitsController extends Controller
 
     public function addAction()
     {
-        return $this->render('FFFastBundle:Gammes:add.html.twig');
+    	$gammeproduit = new GammeProduit():
+
+    	$formBuilder = $this->get('form.factory')->createBuilder(FormType::class,GammeProduit):
+
+    	$formBuilder
+	      ->add('date',      DateType::class)
+	      ->add('name',     TextType::class)
+	      ->add('description',   TextareaType::class)
+	      ->add('save',      SubmitType::class)
+	      ;
+
+
+	      $form = $formBuilder->getForm();
+
+        return $this->render('FFFastBundle:Gammes:add.html.twig', array(
+        	'form' => $form->createView(),
+        	));
     }
 }
