7881ee355d1f7cd9d9c96511e4dba2fe5a6140e2
lebenef <lebenef@ssh4>
2018-02-15 22:14:25 +0100

    Crud

--- src/FF/FastBundle/Controller/GammesController.php
+++ src/FF/FastBundle/Controller/GammesController.php
@@ -3,102 +3,128 @@
 namespace FF\FastBundle\Controller;
 
 use FF\FastBundle\Entity\Gammes;
+use FF\FastBundle\Form\GammesType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class GammesController extends Controller
 {
     public function indexAction()
     {
-
-    $listGammes   = array(
-      array(
-        'name'   => 'Pizza',
-        'id'      => 1,
-        'description'  => 'ronde',
-        'date'    => new \Datetime()),
-      array(
-        'name'   => 'Agoulou',
-        'id'      => 2,
-        'description'  => 'rond',
-        'date'    => new \Datetime()),
-      array(
-        'name'   => 'Bokit',
-        'id'      => 3,
-        'description'  => 'rond',
-        'date'    => new \Datetime())
-    );
+					$repository = $this->getDoctrine()
+						->getManager()
+						->getRepository('FFFastBundle:Gammes')
+						;
+					$listGammes = $repository->findAll();
+			
 
         return $this->render('FFFastBundle:Gammes:index.html.twig', array('listGammes' => $listGammes) );
     }
 
-    public function addAction()
+    public function addAction(Request $request)
     {
-    	$gammes = new Gammes();
+   	 	$gammes = new Gammes();					
+   		$form = $this->get('form.factory')->create(GammesType::class, $gammes);
 
-    	$formBuilder = $this->get('form.factory')->createBuilder(FormType::class,$gammes);
+				if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()){
+									$em = $this->getDoctrine()->getManager();
+									$em->persist($gammes);
+									$em->flush();
 
-    	$formBuilder
-	      ->add('date',      DateType::class)
-	      ->add('name',     TextType::class)
-	      ->add('description',   TextareaType::class)
-	      ->add('save',      SubmitType::class)
-	      ;
+				
+        $request->getSession()->getFlashBag()->add('notice', 'Gamme bien enregistrée.');
 
-
-	      $form = $formBuilder->getForm();
+        return $this->redirectToRoute('ff_fast_view', array('id' => $gammes->getId()));
+      }
+    
 
         return $this->render('FFFastBundle:Gammes:add.html.twig', array(
         	'form' => $form->createView(),
         	));
     }
+	
 
     public function viewAction($id)
-      {
-        $gammes = array(
-          'name'   => 'Pizza',
-          'id'      => $id,
-          'description'  => 'ronde',
-          'date'    => new \Datetime()
-        );
-
-        return $this->render('FFFastBundle:Gammes:view.html.twig', array(
+      {			
+					$repository = $this->getDoctrine()
+						->getManager()
+						->getRepository('FFFastBundle:Gammes')
+						;
+					$gammes = $repository->find($id);
+			
+			    if (null === $gammes) {
+      throw new NotFoundHttpException("La gamme d'id ".$id." n'existe pas.");
+					}
+						
+      return $this->render('FFFastBundle:Gammes:view.html.twig', array(
           'gammes' => $gammes
         ));
-      }
-
-
-    public function editAction($id)
-    {
-        $gammes = new Gammes();
-			   $gammes = array(
-          'name'   => 'Pizza',
-          'id'      => $id,
-          'description'  => 'ronde',
-          'date'    => new \Datetime()
-        );
-
-        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class,$gammes);
-
-        $formBuilder
-          ->add('date',      DateType::class)
-          ->add('name',     TextType::class)
-          ->add('description',   TextareaType::class)
-          ->add('save',      SubmitType::class)
-          ;
-
-
-          $form = $formBuilder->getForm();
-
-        return $this->render('FFFastBundle:Gammes:edit.html.twig', array(
-            'form' => $form->createView(),
-					"gammes" => $gammes
-            ));
     }
 
+
+    public function editAction($id,Request $request )
+    {
+    $em = $this->getDoctrine()->getManager();		
+    $gammes = $em->getRepository('FFFastBundle:Gammes')->find($id);
+			
+			if (null === $gammes) 
+			{
+				throw new NotFoundHttpException("La gamme d'id ".$id." n'existe pas.");
+			}
+
+   		$form = $this->get('form.factory')->create(GammesType::class, $gammes);
+
+				if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()){
+									$em = $this->getDoctrine()->getManager();
+									$em->flush();
+
+				
+			$request->getSession()->getFlashBag()->add('notice', 'Gamme bien modifiée.');
+		  return $this->redirectToRoute('ff_fast_view', array('id' => $gammes->getId()));
+					
+				}
+			
+			
+			
+			
+			        return $this->render('FFFastBundle:Gammes:edit.html.twig', array(
+					'gammes'=> $gammes,			
+        	'form' => $form->createView(),
+        	));
+
+      dump($gammes);
+				
+            
+    }
+
+	
+	public function deleteAction(Request $request, $id)
+  {
+    $em = $this->getDoctrine()->getManager();
+
+    $gammes = $em->getRepository('FFFastBundle:Gammes')->find($id);
+
+    if (null === $gammes) {
+      throw new NotFoundHttpException("La Gamme d'id ".$id." n'existe pas.");
+    }
+
+    // On crée un formulaire vide, qui ne contiendra que le champ CSRF
+    // Cela permet de protéger la suppression d'annonce contre cette faille
+    $form = $this->get('form.factory')->create();
+
+    if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+      $em->remove($gammes);
+      $em->flush();
+
+      $request->getSession()->getFlashBag()->add('info', "La Gamme a bien été supprimée.");
+
+      return $this->redirectToRoute('ff_fast_home');
+    }
+    
+    return $this->render('FFFastBundle:Gammes:delete.html.twig', array(
+      'gammes' => $gammes,
+      'form'   => $form->createView(),
+    ));
+  }
 }
