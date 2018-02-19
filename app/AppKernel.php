938786758e17e4325b442d3384188a2576226ebf
lebenef <lebenef@ssh4>
2018-02-19 22:02:23 +0100

    Indentification

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FF\FastBundle\FFFastBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new FF\UserBundle\FFUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/FF/UserBundle/FFUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FF\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FFUserBundle extends Bundle
+{
+}
